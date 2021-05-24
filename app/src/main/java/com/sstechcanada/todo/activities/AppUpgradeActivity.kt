acfa086e98542a055a139b450e3a8b0536e9420f
Akshay Kumar <mail.akshay04@gmail.com>
2021-05-25 01:38:33 +0530

    ADD: toggle button for pricing

--- app/src/main/java/com/sstechcanada/todo/activities/AppUpgradeActivity.kt
+++ app/src/main/java/com/sstechcanada/todo/activities/AppUpgradeActivity.kt
@@ -4,12 +4,21 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make
+import com.savvyapps.togglebuttonlayout.Toggle
+import com.savvyapps.togglebuttonlayout.ToggleButtonLayout
 import com.sstechcanada.todo.R
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_app_upgrade.*
 
 class AppUpgradeActivity : AppCompatActivity() {
+
     val premium_app = "com.sstechcanada.todo_plus"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_upgrade)
@@ -23,5 +32,24 @@ class AppUpgradeActivity : AppCompatActivity() {
         }
 
         fabBack.setOnClickListener { super.onBackPressed() }
+        setupPriceToggle()
+
     }
+
+    private fun setupPriceToggle() {
+        toggle_button_layout.setToggled(2131362117, true)
+        toggle_button_layout.onToggledListener = { toggleButton: ToggleButtonLayout, toggle: Toggle, b: Boolean ->
+
+            when {
+                toggle.title.toString() == getString(R.string._0_99_month) -> {
+                    tvListsCount.text = getString(R.string.create_up_to_3_to_do_lists)
+                }
+                toggle.title.toString() == getString(R.string._9_99_month) -> {
+                    tvListsCount.text = getString(R.string.create_up_to_20_to_do_lists)
+                }
+            }
+        }
+
+    }
+
 }
