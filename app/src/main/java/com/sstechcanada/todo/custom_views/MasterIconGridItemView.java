d33434ea7294818c7d4318b4ecd9e9f6f01a807a
vaibhavriju <Vaibhavrijumishra@gmail.com>
2021-06-04 09:27:42 +0530

    fixed cardview in masterTodoIconGridView.

--- app/src/main/java/com/sstechcanada/todo/custom_views/MasterIconGridItemView.java
+++ app/src/main/java/com/sstechcanada/todo/custom_views/MasterIconGridItemView.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.cardview.widget.CardView;
 
@@ -12,20 +12,20 @@ import com.sstechcanada.todo.R;
 
 public class MasterIconGridItemView extends FrameLayout {
 
-    private TextView textView;
+    private ImageView iconImageView;
     private CardView cardView;
 
     public MasterIconGridItemView(Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.item_grid_master_list, this);
-        textView = (TextView) getRootView().findViewById(R.id.text);
+        iconImageView = (ImageView) getRootView().findViewById(R.id.iconImageView);
         cardView = (CardView) getRootView().findViewById(R.id.cardView);
     }
 
     public void display(int text,boolean isSelected) {
 //        int imageResource = getContext().getResources().getIdentifier(text, null, getContext().getPackageName());
         Drawable drawable = getContext().getResources().getDrawable(text);
-        textView.setBackground(drawable);
+        iconImageView.setBackground(drawable);
 //        textView.setText(text);
         display(isSelected);
     }
